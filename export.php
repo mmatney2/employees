35a4d862253d6e68520a2a3fa3cecbe80483f9fc
Marquita Matney <marquita.matney@gmail.com>
2022-09-08 08:36:39 -0400

    first commit

--- export.php
+++ export.php
@@ -2,42 +2,46 @@
 // Checking data by post method
 if(isset($_POST["export"])) {
 	// Connect to our data base
-	$con = mysqli_connect("localhost",
-		"root", "", "employees");
+	$con = mysqli_connect("localhost","root", "", "employees");
 	// Accept csv or text files
 	header('Content-Type: text/csv; charset=utf-8');
 	// Download csv file as geeksdata.csv
 	header('Content-Disposition: attachment;filename=employees.csv');
 	// Storing data
-	$output = fopen("php://output", "w"); 
-	fputcsv($output, array('Marketing', 'IST', 'Accounting'));
-	$sql = "SELECT SUM(salary) FROM `employees` GROUP BY `dept` DESC;";
+	$output = fopen("php://output", "w+"); 
 
-	// fputcsv($output, array('Totals By State'));
-	$sql .= "SELECT SUM(salary) 'Totals By State' FROM `employees` GROUP BY `state` ASC;";
+   //for retreiving totals by dept.
+	$sql  = "SELECT 'Totals By Dept :' FROM `employees` limit 1; ";
+	$sql .= "SELECT `dept`, FORMAT(CAST(SUM(salary) as Decimal(13,2)),2)  FROM `employees` GROUP BY `dept` DESC;";
+	$sql .= "SELECT null FROM `employees` limit 1; ";
 
-	// fputcsv($output, array('Overall Average Salary'));
-	$sql .= "SELECT ROUND(AVG(salary), 2) 'Overall Average Salary' FROM `employees`;";
+   
+	//for retreiving totals by states.
+	
+	$sql .= "SELECT 'Totals By State :' FROM `employees` limit 1; ";
+	$sql .= "SELECT `state`, FORMAT(CAST(SUM(salary) as Decimal(13,2)),2)  FROM `employees` GROUP BY `state` ASC;";
+	$sql .= "SELECT null FROM `employees` limit 1; ";
 
-	// fputcsv($output, array('Overall Total Salary'));
-	$sql .= "SELECT SUM(salary) 'Overall Total Salary' FROM `employees`;";
+	// //for retreiving overall average salary
+	$sql .= "SELECT 'Overall Average Salary :', FORMAT(CAST(AVG(salary) as Decimal(13,2)),2) FROM `employees`;";
+	// for retreiving overall total salary
+	$sql .= "SELECT 'Overall Total Salary :', FORMAT(CAST(SUM(salary) as Decimal(13,2)),2) FROM `employees`; ";
+	$sql .= "SELECT null FROM `employees` limit 1; ";
 
-	// fputcsv($output, array('Employees Alphabetic'));
-	$sql .= "SELECT `name` 'Employees Alphabetic' FROM `employees`;";
+	//Employees Alphabetical
+	$sql .= "SELECT 'Employees Alphabetic:' FROM `employees` limit 1; ";
+	$sql .= "SELECT null , `name` FROM `employees` ORDER BY `name` ASC;";
 	// Execute multi query
+	$lines = [];
 	if (mysqli_multi_query($con, $sql)) {
-		do {
+		do { 
 		// Store first result set
-		if ($result = mysqli_store_result($con)) {
-			while ($row = mysqli_fetch_row($result)) {
-			printf("%s\n", $row[0]);
-			}
+		if ($result = mysqli_store_result($con)) {		
+			while ($row = mysqli_fetch_assoc($result)) {
+				fputcsv($output, array_values($row));	
+			}			
 			mysqli_free_result($result);
 		}
-		// if there are more result-sets, the print a divider
-		if (mysqli_more_results($con)) {
-			printf("-----------\n");
-		  }
 		//Prepare next result set
 		} while (mysqli_next_result($con));
 	}
